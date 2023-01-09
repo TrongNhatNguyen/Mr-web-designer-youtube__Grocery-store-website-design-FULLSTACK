a9f42d920ebdde76b29502c20541e397a58cb86a
Nguyen Trong Nhat <nguyentrongnhat240700@gmail.com>
2023-01-09 20:58:24 +0700

    thu gọn đường dẫn URL(bỏ public,index) + setup cơ bản!

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://mylocalapp-codeigniter.test/Mr-web-designer-youtube-Grocery-store-Hugovesta/';
+    // public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
@@ -56,7 +57,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $uriProtocol = 'REQUEST_URI';
+    public $uriProtocol = 'PATH_INFO';
+    // public $uriProtocol = 'REQUEST_URI';
 
     /**
      * --------------------------------------------------------------------------
